60e2be35ba5835b8972c6219d69af144a57bfa8f
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-12-04 15:40:14 -0800

    created a components folder to hold all my components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './Home'
-import Nav from './Nav'
-import BlogEntry from './BlogEntry'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import BlogEntry from './components/BlogEntry'
 import './App.css';
 
 class App extends Component {
