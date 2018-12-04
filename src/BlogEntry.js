a6f53ba761ce7f891def6a45cc5638ce94cbae70
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-12-04 14:11:54 -0800

    added blog entry stub and homepage stub

---
+++ src/BlogEntry.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class BlogEntry extends Component {
+  
+  render() {
+    return (
+      <div>
+       <p>This is where the Text editor will be found</p>
+      </div>
+    );
+  }
+}
+
+export default BlogEntry;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Home extends Component {
+  
+  render() {
+    return (
+      <div>
+       <h1>This will be the main page</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
